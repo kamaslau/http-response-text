bf362c18707e9cae2bfa3932fabe41982ed186ed
Kamas Lau <kamaslau@outlook.com>
2024-12-04 11:42:36 +0800

    refactor: donot use dynamic import anymore

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,10 @@
+import { dict as en } from './dicts/en.js';
+let dicts = { en };
 export const getStatus = async (code = 404, lang = 'en') => {
     const result = {
         code,
-        message: await lookUp(lang)
+        message: dicts[lang]?.[code]
     };
-    console.log('result: ', result);
-    return result;
-};
-const lookUp = async (lang = 'en') => {
-    const filePath = `dicts/${lang}.js`;
-    const { dict } = await import(filePath);
-    const result = dict[lang] ?? 'Un-known status code';
     console.log('result: ', result);
     return result;
 };
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
+import { dict as en } from './dicts/en.js'
+
+let dicts = { en }
+
 export const getStatus = async (code: statusCode = 404, lang: languageCode = 'en'): Promise<outputContents> => {
   const result = {
     code,
-    message: await lookUp(lang)
+    message: dicts[lang]?.[code]
   }
 
-  console.log('result: ', result)
-  return result
-}
-
-const lookUp = async (lang: languageCode = 'en'): Promise<statusMessage> => {
-  const filePath = `dicts/${lang}.js`
-  const { dict } = await import(filePath)
-  const result = dict[lang] ?? 'Un-known status code'
-
-  console.log('result: ', result)
   return result
 }
